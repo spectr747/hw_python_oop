97137087ffb9e59257a1277970dab7512e6b9353
Alim Gubzhokov <gubzh@mail.ru>
2021-11-23 22:46:03 +0300

    Add class with information message - InfoMessage

--- homework.py
+++ homework.py
@@ -1,6 +1,30 @@
 class InfoMessage:
     """Информационное сообщение о тренировке."""
-    pass
+    
+    def __init__(self,
+                training_type: str,
+                duration: float,
+                distance: float,
+                speed: float,
+                calories: float,
+                ) -> None:
+        self.training_type  = training_type
+        self.duration       = duration
+        self.distance       = distance
+        self.speed          = speed
+        self.calories       = calories
+
+    def get_message(self) -> str:
+        """Содержание сообщения."""
+
+        message = (
+                    f'Тип тренировки: {self.training_type}; '
+                    f'Длительность: {self.duration:.3f} ч.; '
+                    f'Дистанция: {self.distance:.3f} км; '
+                    f'Ср. скорость: {self.speed:.3f} км/ч; '
+                    f'Потрачено ккал: {self.calories:.3f}.'
+        )
+        return message
 
 
 class Training:
